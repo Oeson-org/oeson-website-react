1df91d83a40630474d93fb025d841b752dea558c
Anshu-webDev <anshubhagat201@gmail.com>
2022-05-16 12:22:40 +0530

    commented mainsec1 and mainsec2

--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import Header from './header.jsx';
+import Header from './../../layout/header';
 import Mainsec1 from './mainsec1.jsx';
 import Ourservices from './ourservices.jsx';
 import Whychooseus from './whychooseus.jsx';
@@ -9,19 +9,19 @@ import Aboutus from './aboutus.jsx';
 
 
 function Home() {
-    return(  
-  <div>
-  < Header />
-  < Mainsec1 />
-  < Mainsec2 />
-  < Mainsec3 />
-  < Cards/>
-  < Aboutus/>
-  
-  
-  </div>   );
-  
-  }
-  
-  export default Home;
-  
+  return (
+    <div>
+      < Header />
+      < Mainsec1 />
+      {/* < Mainsec2 />
+      < Mainsec3 /> */}
+      < Cards />
+      < Aboutus />
+
+
+    </div>);
+
+}
+
+export default Home;
+
